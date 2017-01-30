7d24678b91c4669acbf68975e52085f63aec2f58
Chaitanya Mahasamudram <chaitanya.mahasamudram@truvenhealth.com>
2017-01-30 03:41:37 -0500

    Main print message change

--- src/main/java/com/vm/Util/VMMain.java
+++ src/main/java/com/vm/Util/VMMain.java
@@ -4,7 +4,7 @@ public class VMMain {
 
 	public static void main(String[] args) {
 		System.out.println(
-				"\n\n Run gradle Tests - ./gradlew clean test  - to see test results for Vending Machine Code Kata \n\n");
+				"\n\n Vending Machine Code Kata \n\n");
 	}
 
 }
